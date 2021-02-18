85dfe6c1bf251507cca2007778d24ca4644e66d3
Chris <cjrumble@yahoo.com>
2021-02-17 19:40:33 -0800

    Initial commit

---
+++ src/PerfectNumber.java
@@ -0,0 +1,16 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        if (number <= 0) {
+            return false;
+        }
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return (sum == number);
+    }
+
+}
